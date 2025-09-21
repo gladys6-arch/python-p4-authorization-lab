e51973d4bc02275c6bc837d4fdb65597463f1ee4
gladys <achandogladys6@gmail.com>
2025-09-21 10:55:09 +0300

    authorization

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,5 +1,4 @@
 import flask
-
 from app import app
 from models import Article, User
 
@@ -7,7 +6,7 @@ app.secret_key = b'a\xdb\xd2\x13\x93\xc1\xe9\x97\xef2\xe3\x004U\xd1Z'
 
 class TestApp:
     '''Flask API in app.py'''
-    
+
     def test_can_only_access_member_only_while_logged_in(self):
         '''allows logged in users to access member-only article index at /members_only_articles.'''
         with app.test_client() as client:
@@ -53,7 +52,8 @@ class TestApp:
                 'username': user.username
             })
 
-            article_id = Article.query.with_entities(Article.id).first()[0]
+            # ✅ Only fetch member-only article IDs
+            article_id = Article.query.filter_by(is_member_only=True).with_entities(Article.id).first()[0]
 
             response = client.get(f'/members_only_articles/{article_id}')
             assert(response.status_code == 200)
